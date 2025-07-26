6c22e3f5517a15869a57f2fca0c3bcca31a90da3
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2025-07-27 02:08:22 +0530

    Fix

--- plugins/pluginsCore/misc.js
+++ plugins/pluginsCore/misc.js
@@ -2,33 +2,22 @@ const Jimp = require("jimp");
 
 async function generateProfilePicture(buffer) {
 	const jimp = await Jimp.read(buffer);
-
-	const min = Math.min(jimp.getWidth(), jimp.getHeight());
-	const x = (jimp.getWidth() - min) / 2;
-	const y = (jimp.getHeight() - min) / 2;
-
-	const cropped = jimp.crop(x, y, min, min);
-
-	const fullImage = cropped
-		.clone()
-		.resize(720, 720, Jimp.RESIZE_BEZIER)
-		.quality(100);
-
-	const previewImage = cropped
-		.clone()
-		.resize(96, 96, Jimp.RESIZE_BEZIER)
-		.quality(100);
-
+	const min = jimp.getWidth();
+	const max = jimp.getHeight();
+	const cropped = jimp.crop(0, 0, min, max);
 	return {
-		img: await fullImage.getBufferAsync(Jimp.MIME_JPEG),
-		preview: await previewImage.getBufferAsync(Jimp.MIME_JPEG)
+		img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
+		preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG)
 	};
 }
 
 async function updatefullpp(jid, imag, client) {
-	const { query } = client;
-	const { img } = await generateProfilePicture(imag);
-
+	const {
+		query
+	} = client;
+	const {
+		img
+	} = await generateProfilePicture(imag);
 	await query({
 		tag: "iq",
 		attrs: {
@@ -46,4 +35,5 @@ async function updatefullpp(jid, imag, client) {
 	});
 }
 
-module.exports = { updatefullpp };
+
+module.exports = {updatefullpp};
